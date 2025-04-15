60285210b0fbddb2b39b081f8f81b74ba4b469ab
zjesko <akshat.c2k@gmail.com>
2025-04-15 18:59:23 +0530

    update register rewards script

--- contracts/validator-manager/scripts/RegisterRewards.s.sol
+++ contracts/validator-manager/scripts/RegisterRewards.s.sol
@@ -2,17 +2,10 @@
 pragma solidity 0.8.25;
 
 import {Script} from "forge-std/Script.sol";
-import {ProxyAdmin} from "@openzeppelin/contracts@5.0.2/proxy/transparent/ProxyAdmin.sol";
 import {Native721TokenStakingManager} from "../Native721TokenStakingManager.sol";
-import {ICMInitializable} from "@utilities/ICMInitializable.sol";
-import {console} from "forge-std/console.sol";
-import {StakingManagerSettings} from "../Native721TokenStakingManager.sol";
-import {ValidatorManager} from "../ValidatorManager.sol";
-import {IERC721} from "@openzeppelin/contracts@5.0.2/token/ERC721/IERC721.sol";
-import {ITransparentUpgradeableProxy} from "@openzeppelin/contracts@5.0.2/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {ExampleERC20} from "@mocks/ExampleERC20.sol";
 
-contract UpgradeBEAMStakingManager is Script {
+contract RegisterRewards is Script {
     function run() external {
         vm.startBroadcast();
         
@@ -20,10 +13,10 @@ contract UpgradeBEAMStakingManager is Script {
 
         // ExampleERC20 rewardToken = new ExampleERC20();
         ExampleERC20 rewardToken = ExampleERC20(0xFE42fC7Ac06ad13BD54aA3010E2f5e9e0DF6D752);
-        rewardToken.approve(address(stakingManager), 4000e18);
+        rewardToken.approve(address(stakingManager), 10000e18);
         
-        stakingManager.registerRewards(true, 10084, address(rewardToken), 2000e18);
-        stakingManager.registerRewards(false, 10084, address(rewardToken), 2000e18);
+        stakingManager.registerRewards(true, 10091, address(rewardToken), 1000e18);
+        stakingManager.registerRewards(false, 10091, address(rewardToken), 1000e18);
         
         vm.stopBroadcast();
     }
